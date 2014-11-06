18f6921af2c4e6269c3d3ed114bee57132541e8b
Cassiano Leal <cassianoleal@gmail.com>
2014-11-06 01:18:44 +0000

    Improve metadata, change license to apache2

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,13 @@
 name             'zshell'
 maintainer       'Cassiano Leal'
 maintainer_email 'cassianoleal@gmail.com'
-license          'all_rights'
+license          'Apache v2.0'
 description      'Installs/Configures zsh'
 long_description 'Installs/Configures zsh'
-version          '0.1.0'
+version          '0.1.1'
+
+recipe "zshell", "Install zsh"
+
+%w{ubuntu debian centos redhat amazon scientific fedora}.each do |os|
+  supports os
+end
