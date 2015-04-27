a832f73dde70b38f1c09ea9352b85726c9211c37
Cassiano Leal <cassianoleal@gmail.com>
2015-04-28 00:26:29 +0100

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cassianoleal@gmail.com'
 license          'Apache v2.0'
 description      'Installs/Configures zsh'
 long_description 'Installs/Configures zsh'
-version          '0.2.0'
+version          '0.2.1'
 
 recipe "zshell", "Install zsh"
 
