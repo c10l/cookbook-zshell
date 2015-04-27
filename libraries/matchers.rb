569a8067f1f3052c6c219e940098a446a4f6c747
Cassiano Leal <cassianoleal@gmail.com>
2015-04-27 23:28:36 +0100

    Add Rspec matchers for zhell_antigen LWRP

--- libraries/matchers.rb
+++ libraries/matchers.rb
@@ -8,4 +8,12 @@ if defined?(ChefSpec)
     ChefSpec::Matchers::ResourceMatcher.new(:zshell_rcfile, :delete, resource_name)
   end
 
+  def enable_zshell_antigen(resource_name)
+    ChefSpec::Matchers::ResourceMatcher.new(:zshell_antigen, :enable, resource_name)
+  end
+
+  def disable_zshell_antigen(resource_name)
+    ChefSpec::Matchers::ResourceMatcher.new(:zshell_antigen, :disable, resource_name)
+  end
+
 end
